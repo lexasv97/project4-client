4c2d11f0f838d7dd190d6f612806a860e99c93d1
Aleksei <alexey.sv.97@gmail.com>
2023-11-16 11:22:33 -0500

    12th commit

--- src/pages/LessonDetails.jsx
+++ src/pages/LessonDetails.jsx
@@ -3,24 +3,25 @@ import { useNavigate } from "react-router-dom"
 import { useParams } from "react-router-dom"
 import axios from "axios"
 import { API_URL } from "../services/API_URL";
+import { ReviewCard } from "../components/ReviewCard";
 
 const LessonDetails = () => {
 
-    const [lesson, setLesson] = useState(null)
+  const [lesson, setLesson] = useState(null)
 
-    const { lessonId } = useParams()
+  const { lessonId } = useParams()
 
-    const navigate = useNavigate()
+  const navigate = useNavigate()
 
-    const getLesson = (id) => {
-        axios.get(API_URL + `/lessons/${id}`)
-        .then((response) => {
-            setLesson(response.data)
-        })
-        .catch((err) => {
-            console.log(err)
-          })
-    }
+  const getLesson = (id) => {
+    axios.get(API_URL + `/lessons/${id}`)
+      .then((response) => {
+        setLesson(response.data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
 
 
   useEffect(() => {
@@ -28,22 +29,24 @@ const LessonDetails = () => {
     getLesson(lessonId)
 
   }, [])
- 
+
   return (
     <div>
-        {
-            lesson ?
-            <div className="flex flex-col">
-                <div>
-                    <h1>Lesson details</h1>
-                </div>
-
-            </div>
-            :
+      {
+        lesson ?
+          <div className="flex flex-col">
             <div>
-
+              <h1>Lesson details</h1>
             </div>
-        }
+            <ReviewCard />
+
+          </div>
+          :
+          <div>
+
+          </div>
+
+      }
     </div>
   )
 }
